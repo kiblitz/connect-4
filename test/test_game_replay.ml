60248bab59acf01f170622820b519357e98da435
kiblitz <g.lee13770334@gmail.com>
2024-10-13 22:51:54 -0400

    Add tests for game replay

--- test/test_game_replay.ml
+++ test/test_game_replay.ml
@@ -1,7 +1,7 @@
 open! Core
 open! Import
 
-let%expect_test "no history clear when playing moves that align with future" =
+let%expect_test "simple replay" =
   let t = Board.empty { Game_params.k = 3; width = 4; height = 4 } |> ok_exn in
   print_string (Board.to_string_pretty t);
   [%expect
@@ -51,7 +51,32 @@ let%expect_test "no history clear when playing moves that align with future" =
     +-+-+-+-+
 
     (To_move Blue) |}];
-  let t = Board.undo t |> ok_exn in
+  let record = Board.record t in
+  let t = Board.from_history record |> ok_exn in
+  print_string (Board.to_string_pretty t);
+  [%expect
+    {|
+    +-+-+-+-+
+    | | | | |
+    | | | | |
+    | | | | |
+    | | | | |
+    +-+-+-+-+
+
+    (To_move Red) |}];
+  let t = Board.redo t |> ok_exn in
+  print_string (Board.to_string_pretty t);
+  [%expect
+    {|
+    +-+-+-+-+
+    | | | | |
+    | | | | |
+    | | | | |
+    | |R| | |
+    +-+-+-+-+
+
+    (To_move Blue) |}];
+  let t = Board.redo t |> ok_exn in
   print_string (Board.to_string_pretty t);
   [%expect
     {|
@@ -63,51 +88,20 @@ let%expect_test "no history clear when playing moves that align with future" =
     +-+-+-+-+
 
     (To_move Red) |}];
-  let t = Board.undo t |> ok_exn in
+  let t = Board.redo t |> ok_exn in
   print_string (Board.to_string_pretty t);
   [%expect
     {|
     +-+-+-+-+
     | | | | |
     | | | | |
-    | | | | |
-    | |R| | |
-    +-+-+-+-+
-
-    (To_move Blue) |}];
-  let t = Board.undo t |> ok_exn in
-  print_string (Board.to_string_pretty t);
-  [%expect
-    {|
-    +-+-+-+-+
-    | | | | |
-    | | | | |
-    | | | | |
-    | | | | |
-    +-+-+-+-+
-
-    (To_move Red) |}];
-  let t = Board.place_piece t ~column_idx:1 |> ok_exn in
-  print_string (Board.to_string_pretty t);
-  [%expect
-    {|
-    +-+-+-+-+
-    | | | | |
-    | | | | |
-    | | | | |
-    | |R| | |
+    | | |R| |
+    | |R|B| |
     +-+-+-+-+
 
     (To_move Blue) |}];
   let t = Board.redo t in
   print_s [%message (t : Board.t Or_error.t)];
-  [%expect
-    {|
-    (t
-     (Ok
-      ((board ((0 ()) (1 ((0 Red))) (2 ((0 Blue))) (3 ())))
-       (game_state (To_move Red)) (played_tokens 2)
-       (history ((past (2 1)) (future (2))))
-       (game_params ((k 3) (width 4) (height 4)))))) |}];
+  [%expect {| (t (Error "No next move in history")) |}];
   ()
 ;;
